2472b0c7c2066e9950d12f93039d2286dc8a52f8
Afraym <afraym100herz@gmail.com>
2024-04-11 12:39:59 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(BussinesCardApp());
@@ -7,116 +8,163 @@ void main() {
 class BussinesCardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center ,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            radius: 70,
-            backgroundColor: Colors.white,
-            child: Icon(Icons.phone_iphone , size: 80  , color: Colors.red,),
-            
-          ),
+          backgroundColor: Color.fromARGB(255, 3, 226, 226) ,     
+            body: Padding(
+            padding: const EdgeInsets.all(18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFf1f1f1), shape: BoxShape.circle),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Icon(Icons.arrow_back_ios),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only( top: 10, left : 170),
+                      child: Text(
+                          "Sign In",
+                          style:
+                              TextStyle(color: Color(0xFF393939), fontSize: 18),
+                        ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "T",
+                        style:
+                            TextStyle(color: Color(0xFF601dd5), fontSize: 37),
+                      ),
+                      Text(
+                        "o",
+                        style:
+                            TextStyle(color: Color(0xFF4c3ade), fontSize: 37),
+                      ),
+                      Text(
+                        "D",
+                        style:
+                            TextStyle(color: Color(0xFF4448e6), fontSize: 37),
+                      ),
+                      Text(
+                        "o",
+                        style:
+                            TextStyle(color: Color(0xFF2c66ef), fontSize: 37),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        hintStyle:
+                            TextStyle(fontSize: 14, color: Color(0xFF777B7E)),
+                        hintText: "Enter Email Address/Mobile Number"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: TextField(
+                      decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                          hintStyle:
+                              TextStyle(fontSize: 14, color: Color(0xFF777B7E)),
+                          hintText: "Password",
+                          suffixIcon: Icon(
+                            Icons.lock,
+                            size: 17,
+                          ))),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom : 11),
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: Color(0xFF777B7E)),
+                      ),
+                    ),
+                  ],
+                ),
+                 ElevatedButton( 
+                    onPressed: () {},
+                    child: Text('          Log In          ' , style: TextStyle(color: Colors.white),),
+                    style: ElevatedButton.styleFrom(shape: StadiumBorder() , backgroundColor:Color(0xFF2c66ef) ),
+                  ),
+                
 
-          SizedBox(
-            height: 50,
-            child: Text(
-              "Say Hello To Your New App!",
-              style: TextStyle(
-                  color: Colors.red,
-                  backgroundColor: Colors.white,
-                  fontSize: 18,
-                  fontFamily: 'Pacifico'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top:10 , bottom: 5),
+                  child: Text("Or" , style: TextStyle(fontSize:12.5 ,color: Color(0xFF777B7E) ),),
+                ),
+              ],
             ),
-          ),
-          Text(
-            "You've just saved a week of development and headaches.",
-            style: TextStyle(
-                color: Colors.black,
-                backgroundColor: Colors.white,
-                fontSize: 10),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top : 15 , right: 10 , left: 10 , bottom: 5),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8), color: Colors.white),
-              height: 50,
-              
-              child: Row(
+
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical:4),
+                  child: Text("Log in with social media" , style: TextStyle(fontSize:12.5 ,color:Color(0xFF777B7E) ),),
+                ),
+              ],
+            ),
+             
+             Row(
                mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children :[
+                
+                  IconButton(onPressed: (){}, icon: Icon(FontAwesomeIcons.google) , color: const Color.fromARGB(255, 224, 77, 66), iconSize: 30,),
+                
+                SizedBox(width: 15) ,
+                 IconButton(onPressed: (){}, icon: Icon(Icons.facebook) , color: Colors.blue[900], iconSize: 30,),
+                 SizedBox(width: 15) ,
+                 IconButton(onPressed: (){}, icon: Icon(FontAwesomeIcons.twitter ) , color: Colors.blue, iconSize: 30,),
+                
+              ]
+             ),
+            
+            Padding(
+              padding: const EdgeInsets.only(top :15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10 ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration( color: Colors.red , borderRadius: BorderRadius.circular(15) ),
-                          
-                          height: 50,
-                          width: 200,
-                          child: Center(
-                            child: Text(
-                              'log in',
-                              
-                              style: TextStyle(backgroundColor: Colors.red, color: Colors.white , fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  
+                  Text("New User?" , style: TextStyle(color: Color(0xFF777B7E) , fontSize: 12),),
+                  SizedBox(width: 4) ,
+                  Text("Sign up" , style: TextStyle(color: Colors.blue[900] , fontSize: 14 , decoration: TextDecoration.underline , decorationColor: Colors.blue[900] ,decorationThickness: 2 , decorationStyle: TextDecorationStyle.solid),)
                 ],
               ),
+            )
+
+              ],
             ),
-          ) ,
-           Padding(
-            padding: EdgeInsets.only(top : 5 , right: 10 , left: 10 , bottom: 5),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8), color: Colors.white),
-              height: 50,
-              
-              child: Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10 ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration( color: Colors.red , borderRadius: BorderRadius.circular(15) ),
-                          
-                          height: 50,
-                          width: 200,
-                          child: Center(
-                            child: Text(
-                              'Sign up',
-                              
-                              style: TextStyle(backgroundColor: Colors.red, color: Colors.white , fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  
-                ],
-              ),
-            ),
-          ) ,
-       
-        ],
-      ),
-    ));
+          ),
+        ));
   }
 }
